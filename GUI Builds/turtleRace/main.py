89bd6eb00fdcb6f5a735c9bcc15934a41e524a9d
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-16 18:16:12 +0200

    Pokemon Race

---
+++ GUI Builds/turtleRace/main.py
@@ -0,0 +1,64 @@
+from turtle import *
+import random
+
+screen = Screen()
+screen.setup(width=500,height=400)
+screen.bgcolor('azure')
+
+
+bet = screen.textinput(title='Race Bet!!!', prompt='Which turtle do you think will win? ')
+print(bet)
+
+turtwig = Turtle(shape='turtle')
+torterra = Turtle(shape='turtle')
+squirtle = Turtle(shape='turtle')
+blastoise = Turtle(shape='turtle')
+shuckle = Turtle(shape='turtle')
+
+turtwig.color('lightgreen')
+turtwig.penup()
+turtwig.goto(x=-230,y=0)
+
+torterra.color('Darkgreen')
+torterra.penup()
+torterra.goto(x=-230, y=-60)
+
+squirtle.color('blue')
+squirtle.penup()
+squirtle.goto(x=-230, y=-120)
+
+blastoise.color('black')
+blastoise.penup()
+blastoise.goto(x=-230, y=60)
+
+shuckle.color('red')
+shuckle.penup()
+shuckle.goto(x=-230, y=120)
+
+turtles = [turtwig,torterra,shuckle,squirtle,blastoise]
+
+play = False
+if bet:
+    play = True
+
+while play:
+    
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            play = False
+            
+            if (turtle.pencolor() == bet):
+                print(f"You are the winner!, The winner is {turtle.pencolor()}")
+            else:
+                 print(f"You are the loser!, The winner is {turtle.pencolor()}")
+        
+        turtle.fd(random.choice([0,10]))
+        
+    
+
+
+
+screen.exitonclick()
+
+
+
