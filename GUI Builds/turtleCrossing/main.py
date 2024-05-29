09bdac091f27fe97e2acdadc0d3a0dfbbf3c24c0
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-29 11:13:09 +0200

    Almost Done

--- GUI Builds/turtleCrossing/main.py
+++ GUI Builds/turtleCrossing/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from player import Player
+from obstruction import Obstruction
 import time
 
 screen = Screen()
@@ -9,6 +10,25 @@ screen.title("Why did the turtle cross the road?")
 screen.tracer(0) # Stop refreshing screen
 
 player = Player()
+
+topObstructions = []
+startX = 280
+startY = 252
+for i in range(4):
+    obstruction = Obstruction(startX=startX,startY=startY)
+    topObstructions.append(obstruction)
+    startY -= 58    
+
+bottomObstructions = []
+startX = 280
+startY = 20
+for i in range(5):
+    obstruction = Obstruction(startX=startX,startY=startY)
+    bottomObstructions.append(obstruction)
+    startY -= 58
+    
+obstrunctions = topObstructions + bottomObstructions
+
 
 screen.listen()
 screen.onkeypress(fun=player.moveForward, key='w')
@@ -20,9 +40,20 @@ while play:
     screen.update()
     time.sleep(refereshRate)    
     
+    for o in obstrunctions:
+        o.drive(lowerBound=10, upperBound=60)
+        o.checkPos()
+
+    
     if (player.ycor() > 240):
         player.reset()
-        refereshRate -= 0.01
+        for o in obstrunctions:
+            o.reset()
+            
+        refereshRate -= 0.05
+        
+        if (refereshRate < 0):
+            play = False
     
     if (player.ycor() < -240):
         player.reset()
---
+++ GUI Builds/turtleCrossing/obstruction.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+import random
+
+colors = ['red','blue','maroon','green','yellow', 'silver','spring green', 'cyan', 'purple', 'pink', 'black',
+          'green', 'red', 'blue', 'salmon', 'cyan', 'brown']
+
+
+class Obstruction(Turtle):
+    
+    def __init__(self, startX, startY):
+        super().__init__()
+        
+        self.startX = startX
+        self.startY = startY
+        
+        # What do I need to override
+        self.penup()
+        self.setpos(x=self.startX, y=self.startY)
+        self.color(random.choice(colors))
+        self.shape('square')
+        self.shapesize(stretch_len=4)
+        self.speed('fastest')
+        
+        
+    def drive(self, lowerBound, upperBound):
+        self.backward(random.randint(5,20))
+        
+    def reset(self):
+        self.setpos(x=self.startX, y=self.startY)
+        
+    def checkPos(self):
+        if self.xcor() < -300:
+            self.reset()
+        
+        
