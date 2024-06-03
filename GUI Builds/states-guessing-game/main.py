86697193f3f4fe0c864959e71a7b8470c3abf62f
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-03 18:53:57 +0200

    image issue

--- GUI Builds/states-guessing-game/main.py
+++ GUI Builds/states-guessing-game/main.py
@@ -1,3 +1,10 @@
 import pandas as pd
 from turtle import Turtle, Screen
 
+screen = Screen()
+
+screen.title('Guess the U.S. state')
+image = 'GUI Builds/states-guessing-game/blank_states_img.gif'
+screen.register_shape(image)
+
+screen.exitonclick()
