72c5121f8f537eadcdccfb9198d209e3d7259d69
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-04 23:22:15 +0200

    Draw with turtle

--- GUI Builds/states-guessing-game/main.py
+++ GUI Builds/states-guessing-game/main.py
@@ -1,10 +1,21 @@
 import pandas as pd
 from turtle import Turtle, Screen
+import turtle
 
 screen = Screen()
 
 screen.title('Guess the U.S. state')
 image = 'GUI Builds/states-guessing-game/blank_states_img.gif'
-screen.register_shape(image)
+screen.addshape(name=image,shape=None)
+turtle.shape(image)
 
-screen.exitonclick()
+def getLatLong(x,y):
+    print(x,y)
+
+df = pd.read_csv('GUI Builds/states-guessing-game/50_states.csv')
+
+print(df[['x', 'y']])
+
+
+turtle.onscreenclick(fun=getLatLong)
+turtle.mainloop()
