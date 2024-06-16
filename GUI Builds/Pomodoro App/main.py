7703dfbc49a35c89d666f3a0f8949bc5f93cf3a4
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-16 15:58:18 +0200

    KISS

--- GUI Builds/Pomodoro App/main.py
+++ GUI Builds/Pomodoro App/main.py
@@ -1,20 +1,29 @@
 from systemconstants import *
 from PIL import Image, ImageTk
 import tkinter as tk
-
+from tkinter import ttk
+import time
 
 # Step 1) UI Setup
 
 mainWindow = tk.Tk()
 mainWindow.title("Pomodoro Study App")
+mainWindow.configure(bg="black", padx=0, pady=0)
 
-# 640 * 426
 
+# 640 * 426 Canvas
 canvas = tk.Canvas(width=640, height=426)
+
 gradient_img_jpg = Image.open('GUI Builds/Pomodoro App/gradient.jpg')
 gradient_img = ImageTk.PhotoImage(gradient_img_jpg) 
 
 canvas.create_image(320,213, image=gradient_img)
-canvas.pack()
+canvas.grid(column=1,row=2, sticky=(tk.N, tk.W, tk.E, tk.S))
+timer = canvas.create_text(320, 213, text="00:00", fill="white", font=(FONT_NAME, 50, "bold"))
+timer_label = canvas.create_text(320,100, text="Timer", font=(FONT_NAME, 35, 'bold'), fill="white")
+# canvas.itemconfigure(timer,text="01:00")
+
+# Start btn
+start_btn = ttk.Button(master=mainWindow, text='start', padding=(5,5)).grid(column=1, row=3, sticky=(tk.W))
 
 mainWindow.mainloop()
