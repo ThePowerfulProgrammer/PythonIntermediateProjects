ad1febd9516ac13e9e4b180077956e98c3fb4039
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-27 20:29:45 +0200

    Need to fix columnspan

--- GUI Builds/Password_ManagerTk/main.py
+++ GUI Builds/Password_ManagerTk/main.py
@@ -9,15 +9,34 @@ from PIL import Image, ImageTk
 
 mainWindow = tk.Tk()
 mainWindow.title("Password Generator")
-mainWindow.configure(width=800, height=500, background="white")
+mainWindow.configure(background="white", padx=20, pady=20)
 
-
-mainCanvas = tk.Canvas(width=800,height=500, background="white")
+# canvas
+mainCanvas = tk.Canvas(width=200,height=200, background="white")
+mainCanvas.grid(column=0, row=1)
 img = Image.open("GUI Builds/Password_ManagerTk/logo.png")
 logo = ImageTk.PhotoImage(img)
-mainCanvas.create_image(400,200, image=logo)
+mainCanvas.create_image(100,100, image=logo)
+mainCanvas.grid(row=0, column=1)
 
-mainCanvas.create_text(425,250,text="Text",font=("Oswald", 20, 'bold'))
+# labels
+ApplicationLabel = tk.Label(text="Website:",font=("Courier", 15, 'bold'), background="white").grid(row=1,column=0)
+email_username_Label = tk.Label(text="Email/Username:",font=("Courier", 15, 'bold'), background="white").grid(row=2,column=0)
+passwordLabel = tk.Label(text="Password:",font=("Courier", 15, 'bold'), background="white").grid(row=3,column=0)
 
-mainCanvas.pack()
+# entry
+ApplicationEntry = tk.Entry(width=50, border=5)
+ApplicationEntry.grid(row=1, column=1, sticky=(tk.W))
+email_username_Entry = tk.Entry(width=50, border=5)
+email_username_Entry.grid(row=2, column=1, sticky=(tk.W))
+passwordEntry = tk.Entry(width=21, border=5)
+passwordEntry.grid(row=3, column=1, sticky=(tk.W), pady=0)
+
+# buttons
+generateButton = tk.Button(text="Generate Password", bg="white")
+generateButton.grid(row=3,column=2,sticky=(tk.E))
+
+addButton = tk.Button(text="Add", bg="white", width=50)
+addButton.grid(row=4, column=1, sticky=(tk.W))
+
 mainWindow.mainloop()
