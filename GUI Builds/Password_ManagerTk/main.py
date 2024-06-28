c804ca4f7a08b1861bb6c2bf47485623cec9ed24
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-28 20:37:11 +0200

    Fixed layout apperance

--- GUI Builds/Password_ManagerTk/main.py
+++ GUI Builds/Password_ManagerTk/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk 
-from PIL import Image, ImageTk
+from tkinter import PhotoImage
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -12,31 +12,31 @@ mainWindow.title("Password Generator")
 mainWindow.configure(background="white", padx=20, pady=20)
 
 # canvas
-mainCanvas = tk.Canvas(width=200,height=200, background="white")
-mainCanvas.grid(column=0, row=1)
-img = Image.open("GUI Builds/Password_ManagerTk/logo.png")
-logo = ImageTk.PhotoImage(img)
-mainCanvas.create_image(100,100, image=logo)
+mainCanvas = tk.Canvas(master=mainWindow,width=200,height=200, background="white", borderwidth=0)
+logo_img = PhotoImage(file="GUI Builds/Password_ManagerTk/logo.png")
+mainCanvas.create_image(100,100, image=logo_img)
 mainCanvas.grid(row=0, column=1)
 
 # labels
-ApplicationLabel = tk.Label(text="Website:",font=("Courier", 15, 'bold'), background="white").grid(row=1,column=0)
-email_username_Label = tk.Label(text="Email/Username:",font=("Courier", 15, 'bold'), background="white").grid(row=2,column=0)
-passwordLabel = tk.Label(text="Password:",font=("Courier", 15, 'bold'), background="white").grid(row=3,column=0)
+ApplicationLabel = tk.Label(text="Website:", background="white", font=("Courier", 15)).grid(row=1,column=0)
+email_username_Label = tk.Label(text="Email/Username:", background="white", font=("Courier", 15)).grid(row=2,column=0)
+passwordLabel = tk.Label(text="Password:", background="white", font=("Courier", 15)).grid(row=3,column=0)
 
 # entry
-ApplicationEntry = tk.Entry(width=50, border=5)
-ApplicationEntry.grid(row=1, column=1, sticky=(tk.W))
-email_username_Entry = tk.Entry(width=50, border=5)
-email_username_Entry.grid(row=2, column=1, sticky=(tk.W))
-passwordEntry = tk.Entry(width=21, border=5)
-passwordEntry.grid(row=3, column=1, sticky=(tk.W), pady=0)
+ApplicationEntry = tk.Entry(width=52, border=2)
+ApplicationEntry.grid(row=1, column=1, columnspan=2)
+
+email_username_Entry = tk.Entry(width=52, border=2)
+email_username_Entry.grid(row=2, column=1, columnspan=2)
+
+passwordEntry = tk.Entry(width=33, border=2)
+passwordEntry.grid(row=3, column=1)
 
 # buttons
 generateButton = tk.Button(text="Generate Password", bg="white")
-generateButton.grid(row=3,column=2,sticky=(tk.E))
+generateButton.grid(row=3,column=2)
 
-addButton = tk.Button(text="Add", bg="white", width=50)
-addButton.grid(row=4, column=1, sticky=(tk.W))
+addButton = tk.Button(text="Add", bg="white", width=44)
+addButton.grid(row=4, column=1, columnspan=2)
 
 mainWindow.mainloop()
