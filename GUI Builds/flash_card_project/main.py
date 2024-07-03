3d787dca59fad779bf1ff2829e284d958600b7b9
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-07-03 11:27:58 +0200

    Added the switch card functionality

--- GUI Builds/flash_card_project/main.py
+++ GUI Builds/flash_card_project/main.py
@@ -1,19 +1,40 @@
 import tkinter as tk
 from tkinter import PhotoImage
 import pandas as pd
-
+import time
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 MY_BACKGROUND_COLOR = "#86FEB8" 
-current_word = 0
+switch = 0
+currentWord = 0
 
 # read in the csv
 dataframe = pd.read_csv("./data/Af_Eng_Top_100 - Sheet1.csv")
 
 # functions
+def switchCard():
+    global switch
+    global currentWord
+    if (switch == 1):
+        Canvas.itemconfig(current_img, image=front_card_img)
+        Canvas.itemconfig(language_label, text="Afrikaans")
+        Canvas.itemconfig(word_label, text=dataframe['Afrikkans'].values[currentWord])
+        switch = 0
+        mainWindow.after(1000, switchCard)
+    else:
+        Canvas.itemconfig(current_img, image=back_card_img)
+        Canvas.itemconfig(language_label, text="English")
+        Canvas.itemconfig(word_label, text=dataframe['English'].values[currentWord])
+        switch = 1
+        if (currentWord == 50):
+            return "Game Complete"
+        currentWord += 1
+        mainWindow.after(1000, switchCard)
 
+        
 
+    
 
 # a) Create the GUI
 
@@ -24,11 +45,14 @@ mainWindow.configure(background=MY_BACKGROUND_COLOR, padx=50, pady=50)
 
 # 2) --> Create the flash card
 front_card_img = PhotoImage(file="./images/card_front.png")
-frontCanvas = tk.Canvas(master=mainWindow, width=800, height=526, background=MY_BACKGROUND_COLOR, highlightthickness=0)
-frontCanvas.create_image(400,263, image=front_card_img)
-frontCanvas.grid(row=0,column=0, columnspan=2)
-front_title_text = frontCanvas.create_text(400,150, text="Afrikaans", font=("Arial", 40, "italic"))
-front_af_word = frontCanvas.create_text(400, 263, text=dataframe['Afrikkans'].values[0], font=("Arial", 60, 'bold'))
+back_card_img = PhotoImage(file="./images/card_back.png")
+
+Canvas = tk.Canvas(master=mainWindow, width=800, height=526, background=MY_BACKGROUND_COLOR, highlightthickness=0)
+current_img = Canvas.create_image(400,263, image=front_card_img)
+language_label = Canvas.create_text(400,150, text="Afrikaans", font=("Arial", 40, "italic"))
+word_label = Canvas.create_text(400, 263, text=dataframe['Afrikkans'].values[currentWord], font=("Arial", 60, 'bold'))
+Canvas.grid(row=0,column=0, columnspan=2)
+
 
 
 
@@ -43,13 +67,7 @@ rightBtn.grid(row=1, column=1)
 
 
 
-
-
-
-
-
-
-
+mainWindow.after(1000, switchCard)
 
 
 mainWindow.mainloop()
