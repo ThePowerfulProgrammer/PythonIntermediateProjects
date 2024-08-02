55090af87f9481c2d8430324150adf764fa7c26a
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-08-02 16:44:52 +0200

    Working with access tokens now

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,1 +1,40 @@
+import requests
+from secret import amadeus_endpoint
+
+
+flight_finder_parameters = {
+    "originLocationCode": "SYD",
+    "destinationLocationCode": "BKK",
+    "departureDate": "2024-08-20",
+    "adults":1,
+    "maxPrice": 300
+    
+}
+
+flight_finder_headers = {
+    "authorization": f"Bearer w8ANPU1nYTnkhjLMbGInjGPnZZWz"
+}
+response = requests.get(url="https://test.api.amadeus.com/v2/shopping/flight-offers", params=flight_finder_parameters, headers=flight_finder_headers)
+print(response.raise_for_status())
+
+
+json = response.json()
+print(json)
+
+
+
+
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+# Find amazing flight deals
+# Api's needed:
+#     Flight finder API --> Find Flights
+#     Google sheets API --> How to find flights
+#     Gmail API --> Send an email to make me aware
+
+
+# Google sheet: City, IATA Code, Lowest Price
+
+
+
---
+++ flight-deals/secret.py
@@ -0,0 +1,25 @@
+import requests
+
+amadeus_app_name = "Flight_Deal_Finder"
+amadeus_endpoint = "https://test.api.amadeus.com/v2"
+amadeus_api_key = "DBicg14VKttT9G9fTDr4C0dH3oS6TIim"
+amadeus_api_secret = "L3bVf0IYAGGo8ecS"
+
+
+if __name__ == "__main__":
+        
+    # params = {
+    #     "grant_type":"client_credentials",
+    #     "client_id": amadeus_api_key,
+    #     "client_secret": amadeus_api_secret
+    # }
+
+    # header = {
+    #     "content-type": f"application/x-www-form-urlencoded"
+    # }
+    # response = requests.post(url="https://test.api.amadeus.com/v1/security/oauth2/token", data=params,headers=header)
+    # response.raise_for_status()
+
+    # print(response.text)
+    
+    pass
