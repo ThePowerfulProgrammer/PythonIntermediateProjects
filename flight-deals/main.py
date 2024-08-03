0cebea016143e48b541dbb7ef666cb8255a70e55
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-08-03 14:09:44 +0200

    Almost done

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -17,74 +17,44 @@ flight_finder_parameters = {
 print("Token ",amadeus_access_token)
 
 flight_finder_headers = {
-    "authorization": f"Bearer token here"
+    "authorization": f"Bearer "
 }
 response = requests.get(url="https://test.api.amadeus.com/v2/shopping/flight-offers", params=flight_finder_parameters, headers=flight_finder_headers)
 print(response.raise_for_status())
-
-
 json = response.json()
-
-# print(json['data'][0]['source'])
-
-# print(json['data'][0]['price']['currency'])
-
-# print(json['data'][0]['price']['total'])
-
-# print(json['data'][0]['price']['grandTotal'])
-
-# print("Type: ",json['data'][0]['type'])
-# print("id: " ,json['data'][0]['id'])
-# print("Source: ",json['data'][0]['source'])
-# print("Instant Ticket: ",json['data'][0]['instantTicketingRequired'])
-# print("oneWay: " ,json['data'][0]['oneWay'])
-# print("isUpSellOffer: " ,json['data'][0]['isUpsellOffer'])
-# print("last Ticketing date: " ,json['data'][0]['lastTicketingDate'])
-# print("ltdt: " ,json['data'][0]['lastTicketingDateTime'])
-# print("number of bookable seats: " ,json['data'][0]['numberOfBookableSeats'])
-# print("itineraries: " , json['data'][0]['itineraries'])        
-
+     
 
 flight_deals_table = PrettyTable()
-flight_deals_table.field_names = ["Departure City", "Arrival City", "Currency Code","Cost (ZAR)", "oneWay",
-                                  "Seats Available"]
-
-    #   'itineraries': [
-    #     {
-    #       'duration': 'PT2H25M',
-    #       'segments': [
-    #         {
-    #           'departure': {
-    #             'iataCode': 'DUR',
-    #             'at': '2024-08-20T08:40:00'
-    #           },
-    #           'arrival': {
-    #             'iataCode': 'CPT',
-    #             'at': '2024-08-20T11:05:00'
-    #           },
-    #           'carrierCode': '5Z',
-    #           'number': '901',
-    #           'aircraft': {
-    #             'code': 'CR1'
-    #           },
-    #           'operating': {
-    #             'carrierCode': '5Z'
-    #           },
-    #           'duration': 'PT2H25M',
-    #           'id': '15',
-    #           'numberOfStops': 0,
-    #           'blacklistedInEU': False
-    #         }
-    #       ]
-    #     }
-    #   ],
+flight_deals_table.field_names = ["Departure City", 
+                                 "Departure DateTime",
+                                  "Arrival City",
+                                  "Arrival DateTime",
+                                  "Final PD",
+                                  "  ",
+                                  "Currency Code",
+                                  "Cost",
+                                  "oneWay",
+                                  "Seats Available",
+                                  "Carrier"]
+    
 
 for i in range(17):
-    flight_deals_table.add_row(["DBN", "CPT",
+    flight_deals_table.add_row([f"{json['data'][i]['itineraries'][0]['segments'][0]['departure']['iataCode']}",
+                                f"{ json['data'][i]['itineraries'][0]['segments'][0]['departure']['at']}", 
+                                
+                                f"{json['data'][i]['itineraries'][0]['segments'][0]['arrival']['iataCode']}",
+                                f"{json['data'][i]['itineraries'][0]['segments'][0]['arrival']['at']}",
+                                
+                                f"{json['data'][i]['lastTicketingDate']}",
+                                "  ",
+                                
                                 f"{json['data'][i]['price']['currency']}", 
+                                
                                 f"{json['data'][i]['price']['grandTotal']}", 
                                 f"{not(json['data'][i]['oneWay'])}",
+                                
                                 f"{json['data'][i]['numberOfBookableSeats'] }",
+                                f"{json['dictionaries']['carriers'][ json['data'][i]['itineraries'][0]['segments'][0]['carrierCode']  ]}",                                
                                 
                                 ])
     
