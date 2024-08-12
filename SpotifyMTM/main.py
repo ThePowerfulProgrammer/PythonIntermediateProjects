5cc3605bc93d24c8b65d57b4d0b99137058ac781
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-08-12 18:28:13 +0200

    Using spotify has never been easier

--- SpotifyMTM/main.py
+++ SpotifyMTM/main.py
@@ -1,13 +1,30 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 import requests
-from .secret import CLIENT_ID,CLIENT_SECRET
-
-date = input("Which uear do you want to travel to? Type the date in this format YYYY-MM-DD: ").upper()
+from secret import CLIENT_ID,CLIENT_SECRET
 
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}/")
 
-soup = BeautifulSoup(markup=response.text)
+# date = input("Which uear do you want to travel to? Type the date in this format YYYY-MM-DD: ").upper()
+# response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}/")
+# soup = BeautifulSoup(markup=response.text)
 
-print(soup.prettify())
+
+# scope = "user-library-read"
+
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri="http://example.com"))
+
+# results = sp.current_user_saved_tracks()
+# for index, item in enumerate(results['items']):
+#     track = item['track']
+#     print(index, track['artists'][0]['name'], " - ", track['name'])
+    
+    
+scope = "playlist-modify-private"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri="http://example.com"))
+
+sp.user_playlist_create(user="qljdlbk9slxdci2wcp6t8qths", name="Test", public=False)
+
 
