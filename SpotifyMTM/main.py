f605aa08249e0cb78c893f94dcde2f5d00504a74
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-08-13 19:38:55 +0200

    updates

--- SpotifyMTM/main.py
+++ SpotifyMTM/main.py
@@ -1,8 +1,6 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 import requests
-from secret import CLIENT_ID,CLIENT_SECRET
+
 
 
 
@@ -11,20 +9,22 @@ from secret import CLIENT_ID,CLIENT_SECRET
 # soup = BeautifulSoup(markup=response.text)
 
 
-# scope = "user-library-read"
+def createSongList(date) -> list: 
+    
+    response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}/")
+    soup = BeautifulSoup(response.text,'html.parser')
+    
+    allSongTitles = soup.find_all(name="li ul h3", id="title-of-a-story")
+    for title in allSongTitles:
+        print(title.getText())
+    
+    return []
+    
 
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri="http://example.com"))
-
-# results = sp.current_user_saved_tracks()
-# for index, item in enumerate(results['items']):
-#     track = item['track']
-#     print(index, track['artists'][0]['name'], " - ", track['name'])
+    
+year = "2010-01-20"
+print(createSongList(year))
     
     
-scope = "playlist-modify-private"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri="http://example.com"))
-
-sp.user_playlist_create(user="qljdlbk9slxdci2wcp6t8qths", name="Test", public=False)
 
 
