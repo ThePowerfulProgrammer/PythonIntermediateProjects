4ebb693a371ed3d11613674dffe4292825f980ff
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-08-24 21:45:35 +0200

    Done

--- Capstone_SF_Rent_Research/main.py
+++ Capstone_SF_Rent_Research/main.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from time import sleep
 import requests
-import re
+from secret import google_form, email, password,site
 
 
-response = requests.get(url='https://appbrewery.github.io/Zillow-Clone/')
+response = requests.get(url=site)
 
 
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -32,4 +32,47 @@ for link in links:
     
     
 # I have all my data --> 44 forms that I need to fill.
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(google_form)
+
+# signInBtn = driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[1]/div/div[4]/div/div/a[1]')
+# sleep(2)
+# signInBtn.click()
+
+sleep(2)
+# addressLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+# addressLabel.send_keys(addresses[0].text.strip())
+# rentLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+# rentLabel.send_keys(cleanedPrices[0])
+# linkLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+# linkLabel.send_keys(links[0].get('href'))
+# submitBtn = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+# submitBtn.click()
+
+# sleep(2)
+# newFormLink.click()
+
+for i in range(44):
+    addressLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    rentLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    linkLabel = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submitBtn = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+    print("Filling in form ...")
+    addressLabel.send_keys(addresses[i].text.strip())
+    rentLabel.send_keys(cleanedPrices[i])
+    linkLabel.send_keys(links[i].get('href'))
+    sleep(1)
+    submitBtn.click()
+    sleep(1)
+    newFormLink = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    newFormLink.click()
+    sleep(1)
+
+sleep(3)
+driver.quit()
 
